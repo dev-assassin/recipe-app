d23775ba26f3892c9905c454a043102f43ea81b2
ninjadev1030 <dalianwang1030@yahoo.com>
2018-01-08 14:18:08 +0800

    add recipes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,8 +9,15 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('A test Recipe', 'This is a simple test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-    new Recipe('Another test Recipe', 'This is another simple test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+    new Recipe('A test Recipe',
+      'This is a simple test',
+      'http://www.sailusfood.com/wp-content/uploads/2016/03/veg-momos-recipe.jpg'),
+    new Recipe('Another test Recipe',
+      'This is another simple test',
+      'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
+    new Recipe('Third Recipe',
+      'This is third recipe',
+      'http://deizi23.com/wp-content/uploads/2015/09/Fresh-Recipe-Logo-e1443186115710.jpg')
   ];
 
   constructor() { }
